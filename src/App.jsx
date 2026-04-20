16f9e66a7fb3871763369ad71a8fcf3087cd71b1
Hamza <hamzafarooq49ml@gmail.com>
2026-04-20 11:24:28 +0500

    added react-router and built app-layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import { GlobalStyles } from "./styles/GlobalStyles";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Applayout from "./ui/Applayout";
 
 function App() {
   return (
@@ -8,7 +11,12 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route element={<Applayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/bookings" element={<Bookings />} />
+            <Route path="/cabins" element={<Cabins />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
